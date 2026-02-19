5a23dbda623faf6e149df8b6520e679eb54b6d39
Danylo <danil.bogomazov1356@gmail.com>
2026-02-19 17:38:54 +0200

    Create DataBase

--- db/models.py
+++ db/models.py
@@ -1,1 +1,51 @@
 from django.db import models
+
+
+class Race (models.Model):
+    RACE_FIELD = (
+        ("Ork", "ork"),
+        ("Elf", "elf"),
+        ("Dwarf", "dwarf"),
+        ("Human", "human"),
+    )
+    name = models.CharField(max_length=255, choices=RACE_FIELD, unique=True)
+    description = models.TextField(blank=True)
+
+
+class Skill (models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    bonus = models.CharField(
+        "Describes what kind of bonus players can get from it",
+        max_length=255
+    )
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="skills"
+    )
+
+
+class Guild (models.Model):
+    name = models.CharField(max_length=255, unique=True)
+    description = models.TextField(null=True)
+
+
+class Player (models.Model):
+    nickname = models.CharField(max_length=255, unique=True)
+    email = models.EmailField(max_length=255)
+    bio = models.CharField(
+        "It stores a short description provided "
+        "by a user about himself/herself.",
+        blank=True)
+    race = models.ForeignKey(
+        Race,
+        on_delete=models.CASCADE,
+        related_name="players"
+    )
+    guild = models.ForeignKey(
+        Guild,
+        on_delete=models.SET_NULL,
+        null=True,
+        related_name="players"
+    )
+    created_at = models.DateTimeField(auto_now_add=True)
--- main.py
+++ main.py
@@ -1,11 +1,57 @@
 import init_django_orm  # noqa: F401
+import json
 
 from db.models import Race, Skill, Player, Guild
 
 
+def create_skill(skill: dict, race: Race) -> None:
+    Skill.objects.get_or_create(
+        name=skill.get("name"),
+        bonus=skill.get("bonus"),
+        race=race,
+    )
+
+
+def create_guild(guild: dict) -> None | Guild:
+    if guild is None:
+        return None
+    return Guild.objects.get_or_create(
+        name=guild.get("name"),
+        description=guild.get("description"),
+    )[0]
+
+
+def create_race(race: dict) -> Race:
+    new_race = Race.objects.get_or_create(
+        name=race.get("name"),
+        description=race.get("description"),
+    )[0]
+    for skill in race.get("skills", []):
+        create_skill(skill, new_race)
+    return new_race
+
+
+def create_players(players: dict) -> None:
+    for name, player in players.items():
+        Player.objects.get_or_create(
+            nickname=name,
+            email=player.get("email"),
+            bio=player.get("bio"),
+            race=create_race(player.get("race")),
+            guild=create_guild(player.get("guild")),
+            created_at=player.get("created_at"),
+        )
+
+
 def main() -> None:
-    pass
+    with open("players.json", "r") as file:
+        players = json.load(file)
+    create_players(players)
 
 
 if __name__ == "__main__":
+    Race.objects.all().delete()
+    Skill.objects.all().delete()
+    Guild.objects.all().delete()
+    Player.objects.all().delete()
     main()
